2ec74a3dbe654e547b74ac051a5ce92f21d51f47
DuJjoneKoo <jmg921118@gmail.com>
2026-04-06 21:41:19 +0900

    Level1: 1-2 트랜젝셔널 추가, 닉네임 추가 및 JWT 반영

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -62,7 +62,7 @@ public class TodoService {
                 todo.getModifiedAt()
         ));
     }
-
+    @Transactional
     public TodoResponse getTodo(long todoId) {
         Todo todo = todoRepository.findByIdWithUser(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
